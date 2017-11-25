d2162ef877cc2a5096b991268a5bb4b35101b1d0
pr-1 <prince17.ps6@gmail.com>
2017-11-25 23:16:17 +0530

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
-
+import { ItemsComponent } from './components/items/items.component';
+import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { FormsModule } from "@angular/forms";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase,'ListApp'),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyCnDvJoi8JGm9499CWKgRmW2X_ukXeVyhc",
+  authDomain: "listapp-40b0d.firebaseapp.com",
+  databaseURL: "https://listapp-40b0d.firebaseio.com",
+  projectId: "listapp-40b0d",
+  storageBucket: "listapp-40b0d.appspot.com",
+  messagingSenderId: "1055561542716"
+  }
 };
